c8a9b4bf29b62b5337ed58f766c35fd9a9042903
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-31 09:34:48 -0300

    capturando dados do formulário

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,9 @@
+const botaoAdicionar = document.querySelector(".cadastrar");
 const form = document.querySelector("#novoItem");
-console.log(form);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log(event.target.elements["nome"].value);
+  console.log(event.target.elements["quantidade"].value);
+});
