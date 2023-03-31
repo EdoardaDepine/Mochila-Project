312d547953f38060c2906067081e3a5a19a611fa
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-31 10:18:06 -0300

    salvando os elementos na lista

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,33 @@
 const botaoAdicionar = document.querySelector(".cadastrar");
 const form = document.querySelector("#novoItem");
+const lista = document.querySelector("#lista");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  console.log(event.target.elements["nome"].value);
-  console.log(event.target.elements["quantidade"].value);
+  criaElemento(
+    event.target.elements["nome"].value,
+    event.target.elements["quantidade"].value
+  );
+
+  form.reset();
 });
+
+function criaElemento(nome, quantidade) {
+  console.log(nome);
+  console.log(quantidade);
+
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = quantidade;
+
+  novoItem.appendChild(numeroItem);
+  novoItem.innerHTML += nome;
+
+  lista.appendChild(novoItem);
+
+  console.log(novoItem);
+  console.log(lista);
+}
