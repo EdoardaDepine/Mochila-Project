0f179789d8c285248a28f1466aabebbbe370430a
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-14 09:42:03 -0300

    ajustando o código

--- JS/remover-item.js
+++ JS/remover-item.js
@@ -1,11 +1,13 @@
 const tabela = document.querySelector("#lista");
+const lis = document.querySelectorAll(".item");
 
 tabela.addEventListener("dblclick", function (event) {
   event.target.classList.add("transicaoExclusao");
+  console.log(event.target);
 
   setTimeout(function () {
     event.target.remove();
   }, 500);
 
-  localStorage.removeItem(event.target.remove());
+  const itens = localStorage.getItem("itens");
 });
